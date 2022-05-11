c77a9575f48d8428348ec1dcbfe3df064d8dcad7
Paul M. Summitt <psummitt@gmail.com>
2022-05-11 15:57:30 -0500

    Add files via upload

---
+++ src/SpecialPages/AlexanderandLauraDoraSummit.php
@@ -0,0 +1,194 @@
+<html><!-- #BeginTemplate "/_Templates/SFQ_Page_template.dwt" --><!-- DW6 -->
+<head>
+<!-- #BeginEditable "doctitle" --> 
+<title>Alexander and Laura Dora Summit</title>
+<!-- #EndEditable --> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords"
+content="Summitt, Summit, Sammet, Summet, Spotswood, Frie, Family History, Genealogy, Summitt Family Quarterly, Summitt New Media, Paul M. Summitt, Mary J. Summitt">
+</head>
+
+<body bgcolor="#FFFFFF" background="../_themes/tmres/researchbg.gif" onLoad="">
+<p align="center"><a href="../index.php"><img src="../_themes/tmres/researchbanner.gif" width="600" height="60" name="sfqbanner" alt="The Summitt Family Quarterly" border="0"></a></p>
+<table width="100%" border="0" cellpadding="5" cellspacing="5">
+  <tr> 
+    <td width="15%" rowspan="3" valign="top" align="center"><table width="94%"  border="2" cellspacing="2" cellpadding="2">
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../index.htm">HOME</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../previous_issues.php">Previous Issues</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../FamHistIndex.htm">Family History Index</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../genlinks.php">Links &amp; Resources</a> </font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../SpecialPages.php">Special Pages</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../obitlist.php">Obituaries</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../VirtualCemetery2.php">Virtual Cemetery</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../CardwellCemetery/Cemetery1.php">Cardwell Cemetary Project</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../bdcalc.php">Birthday Calculator</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../blog/blog.htm" target="_blank">Paul's Blog</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif"><a href="../PrivacyPolicy.php">Privacy Policy</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="Arial, Helvetica, sans-serif">Join the <a target="_blank" href="http://communities.msn.com/SummittResearch/"> Summitt Research Community</a></font></strong></h5>
+          </div></td>
+        </tr>
+        <tr>
+          <td bgcolor="#FFFFFF"><div align="center">
+            <h5><strong><font face="century gothic, Arial, Helvetica"> 
+        <script language="Javascript" src="http://map.geoup.com/geoup?template=welcome"></script>
+        </font></strong></h5>
+            </div></td>
+        </tr>
+      </table>
+    </td>
+    <td width="70%" height="42" align="left" valign="top"><!-- #BeginEditable "pagetitle" -->
+      <h2 align="center">Alexander and Laura Dora Summit</h2>
+    <!-- #EndEditable --></td>
+    <td width="15%" rowspan="3"><IMG SRC="http://service.bfast.com/bfast/serve?bfmid=5647408&siteid=31676243&bfpage=sky_pf_search1" BORDER="0" WIDTH="1" HEIGHT="1" NOSAVE >
+<FORM ACTION="http://service.bfast.com/bfast/click" >
+<INPUT TYPE="hidden" NAME="siteid" VALUE="31676243" >
+<INPUT TYPE="hidden" NAME="bfpage" VALUE="sky_pf_search1">
+<INPUT TYPE="hidden" NAME="bfmid" VALUE="5647408" >
+      <table width="120" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+    <tr> 
+      <td colspan="2" align="right"><img src="http://www.ancestry.com/landing/peoplefinder/PF_tab.gif" width="118" height="19"></td>
+    </tr>
+    <tr> 
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/spacer.gif" width="10" height="10"></td>
+    </tr>
+    <tr> 
+      <td>&nbsp;</td>
+      <td><font size="2" face="Verdana, Arial, Helvetica, sans-serif">Find lost 
+        <strong>family</strong>, <strong>friends</strong>, and <strong>loved ones</strong>.</font></td>
+    </tr>
+    <tr> 
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/spacer.gif" width="10" height="25"></td>
+    </tr>
+    <tr> 
+      <td>&nbsp;</td>
+      <td><font color="#0000FF" size="1" face="Verdana, Arial, Helvetica, sans-serif">Enter 
+        a name for immediate results:</font></td>
+    </tr>
+    <tr> 
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/spacer.gif" width="10" height="8"></td>
+    </tr>
+    <tr> 
+      <td width="5">&nbsp;</td>
+      <td width="113" valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><strong>First 
+        Name</strong></font><br> <input name="firstname" type="text" size="13"> 
+      </td>
+    </tr>
+    <tr> 
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/spacer.gif" width="10" height="10"></td>
+    </tr>
+    <tr> 
+      <td width="5">&nbsp;</td>
+      <td width="113" valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><strong>Last 
+        Name</strong></font><br> <input name="lastname" type="text" size="13"> 
+      </td>
+    </tr>
+    <tr> 
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/spacer.gif" width="10" height="10"></td>
+    </tr>
+    <tr> 
+      <td width="5">&nbsp;</td>
+      <td width="113" valign="top">
+        <input type="submit" name="Submit" value="Search">
+      </td>
+    </tr>
+    <tr> 
+      <td>&nbsp;</td>
+      <td height="35" valign="top">&nbsp;</td>
+    </tr>
+    <tr> 
+      <td height="35" colspan="2"><font color="#666666" size="2" face="Arial, Helvetica, sans-serif"><em>&quot;I 
+        found my brother! It's like we've always known each other!&quot;</em></font> 
+      </td>
+    </tr>
+    <tr align="right"> 
+      <td colspan="2"><font color="#999999" size="2" face="Arial, Helvetica, sans-serif"><em>- 
+        Pam from CA</em></font></td>
+    </tr>
+    <tr align="right"> 
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/brother.jpg" width="113" height="108"></td>
+    </tr>
+    <tr align="right">
+      <td colspan="2"><img src="http://www.ancestry.com/landing/peoplefinder/PF_tab.gif" width="118" height="19"></td>
+    </tr>
+</table>
+</FORM>
+</td>
+  </tr>
+  <tr> 
+    <td width="70%" height="42" align="left" valign="top"><!-- #BeginEditable "content" -->
+      <div align="center">Submitted by Glen Summit, 23 Aug 1998&nbsp; </div>
+    <!-- #EndEditable --></td>
+  </tr>
+  <tr>
+    <td width="70%" align="left" valign="top"><!-- #BeginEditable "content2" -->
+
+<meta http-equiv="Content-Language" content="en-us">
+<meta name="GENERATOR" content="Microsoft FrontPage 5.0">
+<meta name="ProgId" content="FrontPage.Editor.Document">
+
+<meta name="keywords"
+content="Summitt, Summit, Sammet, Summet, Spotswood, Frie, Family History, Genealogy, Summitt Family Quarterly, Summitt New Media, Paul M. Summitt, Mary J. Summitt">
+<meta name="Microsoft Border" content="none, default">
+In late July I met with one of Alexander Commodore Summit's two remaining Daughter-in-laws, her name is Helen Summit and she lives in Peabody, Mass and is a native of Lynn, Massachusetts. For those who don't know, Alex was the younger son of Perlive Abernathy, the wife of Isaac L Summit who died at the Battle of Reams Station in Virginia during the Civil War. For more information on Isaac and Perlive Summit, see the Summitt Family Quarterly issue that came out in April. According to Helen, Alexander was a large landowner in Catawba County, North Carolina. Besides owning a farm, he also owned a hardware store in Catawba. There must be some type of information in old phone books about the locality and name of this store. He was financially not hurting, at least up until the great depression; they grew cotton, tobacco, vegetables and owned a number of livestock. She recalled that Alex's wife, Lora Dora Bollinger was often canning vegetables in her spare time. She said after becoming engaged to Alex's son, Willie Summit, here in New England, that the two of them drove down to North Carolina so that Helen could meet Willie's parents and the rest of the family. She said she had the most memorable meeting. She recalled vividly meeting Laura Dora for the first time in the farmhouse's kitchen and that Willie disappeared for some time at the initial meeting to visit friends. Helen recalled that as a New Englander she had the most difficult time understanding Laura Dora, apparently her southern twang was strong. She laughed about one story, she said that after meeting Laura Dora, that she asked Willie what was wrong with her, since she seemed to be spitting up a lot of blood while they were getting acquainted. Willie laughed hard. He said his mother liked to take apple leaves and snuff and then chew the combination. What she was spitting was not blood, but tobacco into her spittoon! This makes me laugh. But Helen had the utmost respect for Laura Dora, she said she was one of the most caring women she had ever met. She described her as a very devoted Baptist, attending with her children at Mt Rahama Baptist Church in Catawba. All of her children including my grandfather Charles E Summit Sr. were raised strong Baptists. She said Alex did not attend church and was a bit of a recluse, but did help to build Mt Rahama Baptist Church though. Both Alex and Laura Dora are both buried there, as well as a number of my ancestors. Once the great depression hit, Alex lost everything. She told me that Willie Summit talked about this and recalled how he and the family were forced to pack up all their belongings and then proceed on a difficult trek by horse and wagon to either Gaston or Garrison County. It was a very grueling transfer for the family, according to her. Apparently later in life, Alex was also a civilian guard at a North Carolina military base and loved to fish. Helen also said that my grandfather got in a tremendous argument with Alex during the 1920's. He up and left. She said Alex was not the easiest man in the world to get along with. My grandfather subsequently joined the Navy and met my grandmother Viola Knowlton at the port of Boston. After completing his service in the Navy, he settled on the North Shore of Boston with Viola in Wenham, Massachusetts, a very quintessential New England town. Here he used his farming skills to work on the Greycroft Farm in the dairy part of the farm. A few years later Willie left North Carolina and joined his brother in Wenham, where they both lived in a two-family house with their families on Friend Court in Wenham. Willie also worked on the same farm, but worked with poultry. In Massachusetts, Willie and Helen had a number of children: Carol, Barbara, Zeke, Charles and Joseph. Charles recently died. Joseph later received the Purple Heart after saving the life of a fellow soldier in Vietnam. Helen still has newspaper clippings framed on her living rooms wall, documenting her son's achievement. Helen provided me with some family pictures and a copy of the original Summit Family Bible documenting all thirteen births of Alex and Laura. She also provided me with an obituary of one of Alex's daughter's, which I will post to the discussion area on the website at a later date.
+<p>&nbsp;</p>
+
+<!-- #EndEditable --></td>
+  </tr>
+</table>
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  <tr>
+    <td> 
+      <hr>      <h5 align="center">For more information, contact <a href="mailto:support@summittnewmedia.com">Webmaster</a>.<br>
+        © 1995-2006 Summitt New Media. All rights reserved. </h5>
+      <p align="center">&nbsp;</p></td>
+  </tr>
+  <!--msnavigation-->
+</table>
+</body>
+<!-- #EndTemplate --></html>
